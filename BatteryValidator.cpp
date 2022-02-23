688e8aa94c4fd8c236860d64b89fb754c694b1d0
Subash-Gopal <91050840+Subash-Gopal@users.noreply.github.com>
2022-02-23 09:37:36 +0530

    Update BatteryValidator.cpp

--- BatteryValidator.cpp
+++ BatteryValidator.cpp
@@ -4,34 +4,28 @@
 #include <assert.h>
 #include "BatteryValidator.h"
 
-BreachType BatteryValidator::inferBreach(double inputValue, double lowerLimit, double upperLimit)
-{
+BreachType BatteryValidator::inferBreach(double inputValue, double lowerLimit, double upperLimit) {
     if (inputValue < lowerLimit) { return TOO_LOW; }
     if (inputValue > upperLimit) { return TOO_HIGH;}
     return NORMAL;
 }
 
-BreachType BatteryValidator::classifyTemperatureBreach(
-    int coolingType, double inputValue)
-{
-    if (coolingType != coolingTypeValidator(coolingType))
-    {
+BreachType BatteryValidator::classifyTemperatureBreach(int coolingType, double inputValue) {
+    if (coolingType != coolingTypeValidator(coolingType)){
         return INVALID;
     }
-    else
-    {
+    else {
         int lowerLimit = limits[coolingType][LOWER_LIMIT_INDEX];
         int upperLimit = limits[coolingType][UPPER_LIMIT_INDEX];
         return inferBreach(inputValue, lowerLimit, upperLimit);
     }
 }
 
-int BatteryValidator::coolingTypeValidator(int coolingType)
-{
+int BatteryValidator::coolingTypeValidator(int coolingType) {
     return (NUMBER_OF_COOLINGTYPES > coolingType)? coolingType : INVALID;
 }
-AlertStatus BatteryValidator::validateBattery()
-{
+
+AlertStatus BatteryValidator::validateBattery() {
     AlertStatus result = ALERTNOTSENT;
     BreachType breachType = classifyTemperatureBreach(getCoolingType(), inputValue);
     result = this->alerterStrategy_->DoAlert(breachType);
